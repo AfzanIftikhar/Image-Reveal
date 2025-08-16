a6d0ea796bda7b7caeae29147c8f7fd4340bdc5a
Afzan Iftikhar <aafzan95@gmail.com>
2025-08-16 23:07:07 +0500

    Image Reveal

---
+++ sript.js
@@ -0,0 +1,14 @@
+gsap.to(".reveal" , {
+    x:1200,
+    delay:2,
+    duration:1.5,
+    ease:"power4.in"
+    
+})
+gsap.from(".image-container img",{
+    scale:2.4,
+    duration:2,
+    ease:"power4.in",
+    delay:0.1
+
+})
